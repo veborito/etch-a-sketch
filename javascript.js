e8997d21538e9b7c63b979bed413e97c1c1224db
Boris Verdecia Echarte <borisverdeciaecharte@gmail.com>
2025-10-23 16:29:57 +0200

    feat: basic sketch pad

--- javascript.js
+++ javascript.js
@@ -1,13 +1,34 @@
-const container = document.querySelector(".container");
-const SIZE = 5;
-
-for (let i = 0; i < SIZE; i++) {
-  let line = document.createElement("div");
-  for (let j = 0; j < SIZE; j++) {
-    let column = document.createElement("div");
-    column.className = "column"
-    line.appendChild(column);
+function make_sketch_pad(size, container) {
+  for (let i = 0; i < size; i++) {
+    const line = document.createElement("div");
+    for (let j = 0; j < size; j++) {
+      const column = document.createElement("div");
+      column.className = "column"
+      column.addEventListener("mouseover", (event) => {
+        event.target.style.backgroundColor = "red";
+      })
+      line.appendChild(column);
+    }
+    line.className = "line";
+    container.appendChild(line);
   }
-  line.className = "line";
-  container.appendChild(line);
 }
+
+
+const container = document.querySelector(".container");
+const sketch_button = document.querySelector("button");
+
+sketch_button.addEventListener("click", ()=> {
+  container.textContent = "";
+  let input = parseInt(prompt("Chose a sketch pad size !"));
+  if (isNaN(input) || input <= 0) {
+    alert("This is not a valid number ! default value 16 used.");
+    input = 16;
+  } else if (input > 100) {
+    alert("Max value 100. 100 used instead");
+    input = 100;
+  }
+  make_sketch_pad(input, container)
+});
+
+make_sketch_pad(16, container)
